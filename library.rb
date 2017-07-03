7c196c7eb6352bdbade60297124dfb447f0f3082
Ross Melville <rossmelville@hotmail.com>
2017-07-03 20:48:36 +0100

    def setup coded in library_specs file for books.

--- library.rb
+++ library.rb
@@ -1,9 +1,8 @@
 class Library
 
-def initialize(title, rental_details)
-  @title = title
-  @rental_details = rental_details
-end
+
+
+
 
 
 end
--- library_specs.rb
+++ library_specs.rb
@@ -3,6 +3,45 @@ require_relative('./library.rb')
 
 class TestLibrary < MiniTest::Test
 
+  def setup
+    @books = [
+      title: "Trainspotting",
+      rental_details: {
+        student_name: "Ross",
+        date: 30-06-2017
+      },
+
+      title: "Ruby for beginners",
+      rental_details: {
+        student_name: "Andy",
+        date: 01-07-2017
+      },
+
+      title: "Harry Potter and the Chamber of Secrets",
+      rental_details: {
+        student_name: "Vicky",
+        date: 12-06-2017
+        },
+
+      title: "When Christ and His Saints Slept",
+      rental_details: {
+        student_name: "Jackson",
+        date: 10-04-2017
+        },
+
+      title: "The Wasp Factory",
+      rental_details: {
+        student_name: "Madison",
+        date: 05-06-2017
+      }
+    ]
+
+  end 
+
+  def test_list_all_books
+    book1 = Book.new("Trainspotting", "Ross", 01-07-2017)
+  end
+
 
 
 
