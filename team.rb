6861555227e93aafe20657cd3fec9549c930d637
MartyCodeClan <zabbalab@hotmail.com>
2017-07-03 16:47:12 +0100

    eighth commit

--- team.rb
+++ team.rb
@@ -1,12 +1,13 @@
 class Team
 
   attr_reader(:name, :players)
-  attr_accessor(:coach)
+  attr_accessor(:coach, :points)
 
-  def initialize(name, players, coach)
+  def initialize(name, players, coach, points)
     @name = name
     @players = players
     @coach = coach
+    @points = points
   end
 
   def get_name
@@ -37,5 +38,17 @@ class Team
     end
     return false
   end
-  
+
+  def points(outcome)
+    if outcome == "win"
+      @points += 3
+    elsif outcome == "loss"
+      @points += 0
+    elsif outcome == "draw"
+      @points += 1
+    else
+      return "not a valid entry"
+    end
+    return @points
+  end
 end
--- team_spec.rb
+++ team_spec.rb
@@ -4,8 +4,8 @@ require_relative('./team')
 class TestTeam < MiniTest::Test
 
   def test_get_name
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team1.get_name
 
@@ -13,8 +13,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_get_players_name
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team2.get_players_names
 
@@ -22,8 +22,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_get_coach_name
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team2.get_coach_name
 
@@ -31,8 +31,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_set_coach_name
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team1.set_coach_name("Coach Maximal")
 
@@ -40,8 +40,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_set_coach_name
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team1.set_coach_name("Coach Maximal")
 
@@ -49,8 +49,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_add_player
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team2.add_player("Maximullion")
 
@@ -58,8 +58,8 @@ class TestTeam < MiniTest::Test
   end
 
   def test_check_player__false
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team1.check_player("Max")
 
@@ -67,12 +67,57 @@ class TestTeam < MiniTest::Test
   end
 
   def test_check_player__true
-    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter")
-    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum")
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
 
     result = team1.check_player("Steve")
 
     assert_equal(true, result)
+  end
+
+  def test_points__win
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
+
+    result = team1.points("win")
+
+    assert_equal(3, result)
+  end
+
+  def test_points__loss
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
+
+    result = team2.points("loss")
+
+    assert_equal(0, result)
+  end
+
+  def test_points__draw
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
+
+    result = team2.points("draw")
+
+    assert_equal(1, result)
+  end
+
+  def test_points__points_on_board
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 3)
+
+    result = team2.points("win")
+
+    assert_equal(6, result)
+  end
+
+  def test_points__invalid
+    team1 = Team.new("Devils", ["Pat", "Steve", "Sam"], "Coach Carter", 0)
+    team2 = Team.new("Spankers", ["Ned", "Whacky", "Covert"], "Coach Wiggum", 0)
+
+    result = team2.points("smashed it")
+
+    assert_equal("not a valid entry", result)
   end
 
 
